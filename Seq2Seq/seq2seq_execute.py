02a9d7bc925875360dc305e2c3627ea1dfb339dc
Suijun <853573918g@gmail.com>
2022-07-24 02:33:35 -0400

    seq2seq_execute add preprocess

--- Seq2Seq/seq2seq_execute.py
+++ Seq2Seq/seq2seq_execute.py
@@ -41,7 +41,37 @@ class Lang:
         else:
             self.word2count[word] += 1
 
-# read data
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+# preprocess data
+def preprocess_sentence(w):
+    w ='start '+ w + ' end'
+    #print(w)
+    return w
+
+def create_dataset(data):
+    pairs = data.loc[:,'Question': "Answer"].values.tolist()
+    pairs_len = len(pairs)
+    i = 0
+    while i < pairs_len:
+        pairs[i] = [preprocess_sentence(pairs[i][0]),preprocess_sentence(pairs[i][1])]
+        i += 1
+
+    input_lang=Lang("ans")
+    output_lang=Lang("ask")
+    pairs = [list(reversed(p)) for p in pairs]
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+
 
 
 pass
+
+create_dataset(train_data_raw)
